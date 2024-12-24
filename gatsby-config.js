043e056fb6312b51f1949df58cfdfc9515c340af
Mark Mei <meiliang1123@126.com>
2024-12-24 16:15:16 +0800

    trigger update

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
         shopifyConnections: ["collections"],
+        apiVersion: "2024-10",
       },
     },
     "gatsby-plugin-image",
