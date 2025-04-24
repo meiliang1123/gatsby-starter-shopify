446e7a4aaaa13e462eb0402121a355cb14391fe4
RaysoLee <raysoli@163.com>
2025-04-24 11:57:53 +0800

    logo & seo

--- src/pages/blog/{ContentfulBlogPost.slug}.jsx
+++ src/pages/blog/{ContentfulBlogPost.slug}.jsx
@@ -1,50 +1,57 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 // import get from 'lodash/get'
-import { renderRichText } from 'gatsby-source-contentful/rich-text'
 import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
-import { BLOCKS } from '@contentful/rich-text-types'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import readingTime from 'reading-time'
 
 import Seo from '../../components/blog/seo'
 // import Layout from '../../components/blog/layout'
+import RichTextRenderer from "@components/RichTextRenderer"
 import Hero from '../../components/blog/hero'
 import Tags from '../../components/blog/tags'
 import * as styles from './blog-post.module.css'
 
 const BlogPostTemplate = ({ data }) => {
-  const { contentfulBlogPost: post, previous, next } = data
-  console.log(data, "博文详情", post)
+  const { contentfulBlogPost: post, allContentfulBlogPost } = data
+    console.log(data, "博文详情=====", post.body)
     // const post = get(this.props, 'data.contentfulBlogPost')
     // const previous = get(this.props, 'data.previous')
     // const next = get(this.props, 'data.next')
+    const currentIndex = allContentfulBlogPost.nodes.findIndex(
+      (node) => node.slug === post.slug
+    )
+    const previous = currentIndex > 0
+      ? allContentfulBlogPost.nodes[currentIndex - 1]
+      : null
+    const next = currentIndex < allContentfulBlogPost.nodes.length - 1
+        ? allContentfulBlogPost.nodes[currentIndex + 1]
+        : null
     const plainTextDescription = documentToPlainTextString(
       JSON.parse(post.description.raw)
     )
     const plainTextBody = documentToPlainTextString(JSON.parse(post.body.raw))
     const { minutes: timeToRead } = readingTime(plainTextBody)
     
-    const options = {
-      renderNode: {
-        [BLOCKS.EMBEDDED_ASSET]: (node) => {
-        const { gatsbyImage, description } = node.data.target
-        return (
-           <GatsbyImage
-              image={getImage(gatsbyImage)}
-              alt={description || ""}
-           />
-         )
-        },
-      },
-    };
+    // const options = {
+    //   renderNode: {
+    //     [BLOCKS.EMBEDDED_ASSET]: (node) => {
+    //     const { gatsbyImage, description } = node.data.target
+    //     return (
+    //        <GatsbyImage
+    //           image={getImage(gatsbyImage)}
+    //           alt={description || ""}
+    //        />
+    //      )
+    //     },
+    //   },
+    // };
 
     return (
       <>
         <Seo
-          title={post.title}
-          description={plainTextDescription}
-          image={`http:${post.heroImage.resize.src}`}
+          title={post.seo?.pageTitle}
+          description={post.seo?.pageDescription?.pageDescription || plainTextDescription}
+          image={`https:${post.seo?.shareImages?.[0]?.file.url}` || `http:${post.heroImage.resize.src}`}
         />
         <Hero
           image={post.heroImage?.gatsbyImage}
@@ -59,7 +66,7 @@ const BlogPostTemplate = ({ data }) => {
           </span>
           <div className={styles.article}>
             <div className={styles.body}>
-              {post.body?.raw && renderRichText(post.body, options)}
+              <RichTextRenderer content={post.body}/>
             </div>
             <Tags tags={post.tags} />
             {(previous || next) && (
@@ -95,10 +102,12 @@ export const pageQuery = graphql`
     $slug: String!
     $previousPostSlug: String
     $nextPostSlug: String
+    $locale: String
   ) {
     contentfulBlogPost(slug: { eq: $slug }) {
       slug
       title
+      node_locale
       author {
         name
       }
@@ -112,18 +121,55 @@ export const pageQuery = graphql`
       }
       body {
         raw
-        
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            id: contentful_id
+            gatsbyImageData(layout: FIXED, width: 264)
+            title
+            description
+          }
+        }
       }
       tags
       description {
         raw
       }
+      seo {
+        internalName
+        pageTitle
+        pageDescription {
+          pageDescription
+        }
+        shareImages {
+          file {
+            url
+          }
+          gatsbyImageData(width: 16)
+        }
+      }
     }
-    previous: contentfulBlogPost(slug: { eq: $previousPostSlug }) {
+    allContentfulBlogPost(
+      filter: { node_locale: { eq: "en-US" } }
+      sort: { publishDate: ASC }
+    ) {
+      nodes {
+        title
+        slug
+        publishDate
+      }
+    }
+    previous: contentfulBlogPost(
+      slug: { eq: $previousPostSlug }
+      node_locale: { eq: $locale }
+    ) {
       slug
       title
     }
-    next: contentfulBlogPost(slug: { eq: $nextPostSlug }) {
+    next: contentfulBlogPost(
+      slug: { eq: $nextPostSlug }
+      node_locale: { eq: $locale }
+    ) {
       slug
       title
     }
