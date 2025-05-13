6769252e301767e939d944c514b528dc0266ef26
RaysoLee <raysoli@163.com>
2025-05-13 15:12:28 +0800

    formatter

--- src/components/AccountButton/index.jsx
+++ src/components/AccountButton/index.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
-import { useAuth0 } from "@auth0/auth0-react";
-import { 
+import { useAuth0 } from '@auth0/auth0-react'
+import {
   Popover,
   PopoverHandler,
   PopoverContent,
@@ -9,25 +9,31 @@ import {
   List,
   ListItem,
   ListItemPrefix,
-} from "@material-tailwind/react";
-import { CgMail, CgMenuBoxed, CgLogOff } from "react-icons/cg";
-import "./index.less"
+} from '@material-tailwind/react'
+import { CgMail, CgMenuBoxed, CgLogOff } from 'react-icons/cg'
+import './index.less'
 
-const defaultAuatarPngAsset = "/images/default_avatar.png"
+const defaultAuatarPngAsset = '/images/default_avatar.png'
 const AccountButton = () => {
-  const {  loginWithRedirect, getAccessTokenSilently, logout, user, isAuthenticated, /* isLoading */ } = useAuth0()
-  const [openPopover, setOpenPopover] = React.useState(false);
+  const {
+    loginWithRedirect,
+    getAccessTokenSilently,
+    logout,
+    user,
+    isAuthenticated /* isLoading */,
+  } = useAuth0()
+  const [openPopover, setOpenPopover] = React.useState(false)
   const [imgSrc, setImgSrc] = useState(defaultAuatarPngAsset)
 
   useEffect(() => {
     if (isAuthenticated) {
       getAccessTokenSilently()
-        .then(token => {
-          console.log("Access token refreshed:=====")
+        .then((token) => {
+          console.log('Access token refreshed:=====')
         })
-        .catch(err => console.error("Error refreshing token:=====", err));
+        .catch((err) => console.error('Error refreshing token:=====', err))
     }
-  }, [isAuthenticated, getAccessTokenSilently]);
+  }, [isAuthenticated, getAccessTokenSilently])
 
   useEffect(() => {
     setImgSrc(user?.picture || defaultAuatarPngAsset)
@@ -36,77 +42,89 @@ const AccountButton = () => {
   const triggers = {
     onMouseEnter: () => setOpenPopover(true),
     onMouseLeave: () => setOpenPopover(false),
-  };
+  }
 
   // 登出
   const onLogout = () => {
     logout({ logoutParams: { returnTo: window.location.origin } })
   }
 
-  return <Popover open={openPopover} handler={setOpenPopover} placement="bottom-end">
-    <PopoverHandler {...triggers} className="cursor-pointer inline-block relative object-cover object-center !rounded-full w-8 h-8 ml-3 rounded-lg">
-      <img
-        alt="avatar"
-        src={imgSrc} onError={() => setImgSrc(defaultAuatarPngAsset)}
-      />
-    </PopoverHandler>
-    <PopoverContent className="w-72 z-[1001]" {...triggers}>
-      {isAuthenticated && <div className="mb-4 flex items-center gap-4 border-b border-blue-gray-50 pb-4">
-          <img 
-            alt="avatar"
-            className="inline-block relative object-cover object-center !rounded-full w-12 h-12 rounded-lg" 
-            src={imgSrc} onError={() => setImgSrc(defaultAuatarPngAsset)}
-          />
-          <div>
-            <Typography variant="h6" color="blue-gray">
-              {user.name}
-            </Typography>
-            <Typography
-              variant="small"
-              color="gray"
-              className="font-medium text-blue-gray-500"
-            >
-              {user?.nickname}
-            </Typography>
+  return (
+    <Popover open={openPopover} handler={setOpenPopover} placement="bottom-end">
+      <PopoverHandler
+        {...triggers}
+        className="cursor-pointer inline-block relative object-cover object-center !rounded-full w-8 h-8 ml-3 rounded-lg"
+      >
+        <img
+          alt="avatar"
+          src={imgSrc}
+          onError={() => setImgSrc(defaultAuatarPngAsset)}
+        />
+      </PopoverHandler>
+      <PopoverContent className="w-72 z-[1001]" {...triggers}>
+        {isAuthenticated && (
+          <div className="mb-4 flex items-center gap-4 border-b border-blue-gray-50 pb-4">
+            <img
+              alt="avatar"
+              className="inline-block relative object-cover object-center !rounded-full w-12 h-12 rounded-lg"
+              src={imgSrc}
+              onError={() => setImgSrc(defaultAuatarPngAsset)}
+            />
+            <div>
+              <Typography variant="h6" color="blue-gray">
+                {user.name}
+              </Typography>
+              <Typography
+                variant="small"
+                color="gray"
+                className="font-medium text-blue-gray-500"
+              >
+                {user?.nickname}
+              </Typography>
+            </div>
           </div>
-        </div>
-      }
-      <List className="p-0">
-        {
-          isAuthenticated && <React.Fragment>
-            <ListItem>
-              <ListItemPrefix>
-                <CgMail />
-              </ListItemPrefix>
-              {user?.email}
-            </ListItem>
-            <Link to="/account">
+        )}
+        <List className="p-0">
+          {isAuthenticated && (
+            <React.Fragment>
               <ListItem>
                 <ListItemPrefix>
-                  <CgMenuBoxed />
+                  <CgMail />
                 </ListItemPrefix>
-                Orders
+                {user?.email}
               </ListItem>
-            </Link>
+              <Link to="/account">
+                <ListItem>
+                  <ListItemPrefix>
+                    <CgMenuBoxed />
+                  </ListItemPrefix>
+                  Orders
+                </ListItem>
+              </Link>
+              <ListItem>
+                <ListItemPrefix>
+                  <CgLogOff />
+                </ListItemPrefix>
+                <button aria-label="log out" onClick={onLogout}>
+                  Log out
+                </button>
+              </ListItem>
+            </React.Fragment>
+          )}
+          {!isAuthenticated && (
             <ListItem>
               <ListItemPrefix>
                 <CgLogOff />
               </ListItemPrefix>
-              <button aria-label="log out" onClick={onLogout}>Log out</button>
+              <button aria-label="log out" onClick={() => loginWithRedirect()}>
+                Sign in
+              </button>
             </ListItem>
-          </React.Fragment>
-        }
-        {
-          !isAuthenticated && <ListItem>
-            <ListItemPrefix>
-              <CgLogOff />
-            </ListItemPrefix>
-            <button aria-label="log out" onClick={() => loginWithRedirect() }>Sign in</button>
-          </ListItem>
-        }
-      </List>
-    </PopoverContent>
-  </Popover>
+          )}
+        </List>
+      </PopoverContent>
+    </Popover>
+  )
 }
 
 export default AccountButton
